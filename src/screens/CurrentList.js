2a6d1e0333402d655d552d054a3f12be90b5e223
voromahery <daniel.her@onja.org>
2021-04-15 12:03:53 +0300

    Applied styles on the input

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import {View, Text, SafeAreaView, ScrollView, Button} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Button,
+  FlatList,
+  KeyboardAvoidingView,
+} from 'react-native';
 import nachos from '../data/nachos';
 import ListItem, {Separator} from '../components/ListItem';
-
+import AddItem from '../components/AddItem';
 export default () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        {nachos.map((item, index) => (
-          <React.Fragment key={item.id}>
+    <SafeAreaView style={{flex: 1}}>
+      <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+        <AddItem />
+        <FlatList
+          data={nachos}
+          renderItem={({item, index}) => (
             <ListItem
               name={item.name}
               onFavoritePress={() => alert('todo: handle favorite')}
               isFavorite={index < 2}
             />
-            <Separator />
-          </React.Fragment>
-        ))}
-      </ScrollView>
+          )}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={() => <Separator />}
+        />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
+{
+  /* <ScrollView>
+{nachos.map((item, index) => (
+  <React.Fragment key={item.id}>
+    <ListItem
+      name={item.name}
+      onFavoritePress={() => alert('todo: handle favorite')}
+      isFavorite={index < 2}
+    />
+    <Separator />
+  </React.Fragment>
+))}
+</ScrollView> */
+}
