6f743a14b4fbfe1f7c7c98848ec7c6c40c84a7d9
voromahery <daniel.her@onja.org>
2021-04-16 11:53:36 +0300

    Cleaned code

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-// import {v4 as uuid} from 'uuid';
-// import AsyncStorage from '@react-native-community/async-storage';
+
 import {
   View,
   Text,
@@ -15,38 +14,9 @@ import nachos from '../data/nachos';
 import ListItem, {Separator} from '../components/ListItem';
 import AddItem from '../components/AddItem';
 import { useCurrentList } from './../util/LIstManager';
-// const updateStoreCurrentList = list => {
-//   AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
-// };
+
 export default () => {
   const {list, loading, addItem, removeItem} = useCurrentList();
-  // const [list, setList] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const addItem = text => {
-  //   const newList = [{id: uuid(), name: text}, ...list];
-  //   setList(newList);
-  //   updateStoreCurrentList(newList);
-  // };
-
-  // const removeItem = id => {
-  //   const newList = list.filter(item => item.id !== id);
-  //   setList(newList);
-  //   updateStoreCurrentList(newList);
-  // };
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     AsyncStorage.getItem('@@GroceryList/currentList')
-  //       .then(data => JSON.parse(data))
-  //       .then(data => {
-  //         if (data) {
-  //           setList(data);
-  //         }
-  //         setLoading(false);
-  //       });
-  //   }, 1000);
-  // }, []);
-
   if (loading) {
     return (
       <SafeAreaView>
