2e0018be41edc233b3dfde4f1276dcb17da8e0f1
voromahery <daniel.her@onja.org>
2021-04-16 11:50:31 +0300

    Refactored code

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {v4 as uuid} from 'uuid';
-import AsyncStorage from '@react-native-community/async-storage';
-
+// import {v4 as uuid} from 'uuid';
+// import AsyncStorage from '@react-native-community/async-storage';
 import {
   View,
   Text,
@@ -15,36 +14,38 @@ import {
 import nachos from '../data/nachos';
 import ListItem, {Separator} from '../components/ListItem';
 import AddItem from '../components/AddItem';
-const updateStoreCurrentList = list => {
-  AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
-};
+import { useCurrentList } from './../util/LIstManager';
+// const updateStoreCurrentList = list => {
+//   AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
+// };
 export default () => {
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const addItem = text => {
-    const newList = [{id: uuid(), name: text}, ...list];
-    setList(newList);
-    updateStoreCurrentList(newList);
-  };
+  const {list, loading, addItem, removeItem} = useCurrentList();
+  // const [list, setList] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const addItem = text => {
+  //   const newList = [{id: uuid(), name: text}, ...list];
+  //   setList(newList);
+  //   updateStoreCurrentList(newList);
+  // };
 
-  const removeItem = id => {
-    const newList = list.filter(item => item.id !== id);
-    setList(newList);
-    updateStoreCurrentList(newList);
-  };
+  // const removeItem = id => {
+  //   const newList = list.filter(item => item.id !== id);
+  //   setList(newList);
+  //   updateStoreCurrentList(newList);
+  // };
 
-  useEffect(() => {
-    setTimeout(() => {
-      AsyncStorage.getItem('@@GroceryList/currentList')
-        .then(data => JSON.parse(data))
-        .then(data => {
-          if (data) {
-            setList(data);
-          }
-          setLoading(false);
-        });
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     AsyncStorage.getItem('@@GroceryList/currentList')
+  //       .then(data => JSON.parse(data))
+  //       .then(data => {
+  //         if (data) {
+  //           setList(data);
+  //         }
+  //         setLoading(false);
+  //       });
+  //   }, 1000);
+  // }, []);
 
   if (loading) {
     return (
---
+++ src/util/LIstManager.js
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react';
+import 'react-native-get-random-values'
+import {v4 as uuid} from 'uuid';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const useCurrentList = () => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const addItem = text => {
+    const newList = [{id: uuid(), name: text}, ...list];
+    setList(newList);
+    updateStoreCurrentList(newList);
+  };
+
+  const updateStoreCurrentList = list => {
+    AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
+  };
+
+  const removeItem = id => {
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+    updateStoreCurrentList(newList);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      AsyncStorage.getItem('@@GroceryList/currentList')
+        .then(data => JSON.parse(data))
+        .then(data => {
+          if (data) {
+            setList(data);
+          }
+          setLoading(false);
+        });
+    }, 1000);
+  }, []);
+
+  return {
+    list,
+    loading,
+    addItem,
+    removeItem,
+  };
+};
