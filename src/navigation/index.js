70e66bcbddd9f31567e2a7b7bcd1e5956928bab5
voromahery <daniel.her@onja.org>
2021-04-19 14:07:34 +0300

    Added different icons

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Image, Platform} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import CurrentList from '../screens/CurrentList';
 import ItemDetails from '../screens/ItemDetails';
+import FavoritesList from '../screens/FavoritesList';
 
 // const CurrentListStack = createStackNavigator({
 //   CurrentList: {
@@ -13,11 +15,11 @@ import ItemDetails from '../screens/ItemDetails';
 
 const Stack = createStackNavigator();
 
-const CurrentListStack = () => {
+export const CurrentListStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="CurrentList" component={CurrentList} />
+        <Stack.Screen name="Shopping List" component={CurrentList} />
         <Stack.Screen
           name="ItemDetails"
           component={ItemDetails}
@@ -33,5 +35,53 @@ const CurrentListStack = () => {
     </NavigationContainer>
   );
 };
-export default CurrentListStack;
+
+const Tab = createBottomTabNavigator();
+
+const FavoritesListStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="FavoritesList" component={FavoritesList} />
+    </Stack.Navigator>
+  );
+};
+const Tabs = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color, focused}) => {
+            let image;
+            if (route.name === 'Shopping List') {
+              image = Platform.select({
+                ios: require('../assets/icons/ios-list.png'),
+                android: require('../assets/icons/md-list.png'),
+              });
+            } else if (route.name === 'FavoriteList') {
+              image = Platform.select({
+                ios: focused
+                  ? require('../assets/icons/ios-star.png')
+                  : require('../assets/icons/ios-star-outline.png'),
+                android: focused
+                  ? require('../assets/icons/md-star.png')
+                  : require('../assets/icons/md-star-outline.png'),
+              });
+            }
+            return (
+              <Image
+                source={image}
+                resizeMode="contain"
+                style={{width: 25, tintColor: color}}
+              />
+            );
+          },
+        })}>
+        <Tab.Screen name="Shopping List" component={CurrentList} />
+        <Tab.Screen name="FavoriteList" component={FavoritesListStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Tabs;
 // export default createAppContainer(CurrentListStack);
---
+++ src/screens/FavoritesList.js
@@ -0,0 +1,10 @@
+// export default () => null;
+
+import React from 'react';
+import {View, Text} from 'react-native';
+export default ({route}) => (
+  <View>
+    {/* <Text>{JSON.stringify(route.params.item, null, 2)}</Text> */}
+    <Text>Favorite</Text>
+  </View>
+);
